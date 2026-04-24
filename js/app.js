d6bce959f633b39d28a6cd5d82bb9943b486ca8e
Felipe Ubirajara <felipeuao2020@gmail.com>
2026-04-24 08:38:00 -0300

    Maneira correta

--- js/app.js
+++ js/app.js
@@ -1,32 +1,17 @@
-function alterarStatusJogo(id) {
-    let botao1 = document.getElementById('game-1');
-    if (botao1.classList.contains('dashboard__item__button>Alugar</a>')) {
-        botao1.classList.remove('dashboard__item__button>Alugar</a>');
-        botao1.classList.add('dashboard__item__button dashboard__item__button--return>Devolver</a>');
-    } else {
-        botao1.classList.remove('dashboard__item__button dashboard__item__button--return>Devolver</a>');
-        botao1.classList.add('dashboard__item__button>Alugar</a>');
-    }
-}
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-function alterarStatusJogo2() {
-    let botao2 = document.getElementById('game-2');
-    if (botao2.classList.contains('dashboard__item__button>Alugar</a>')) {
-        botao2.classList.remove('dashboard__item__buttonv>Alugar</a>');
-        botao2.classList.add('dashboard__item__button dashboard__item__button--return>Devolver</a>');
-    } else {
-        botao2.classList.remove('dashboard__item__button dashboard__item__button--return>Devolver</a>');
-        botao2.classList.add('dashboard__item__button>Alugar</a>');
-    }
-}
 
-function alterarStatusJogo3() {
-    let botao3 = document.getElementById('game-3');
-    if (botao3.classList.contains('dashboard__item__button>Alugar</a>')) {
-        botao3.classList.remove('dashboard__item__button>Alugar</a>');
-        botao3.classList.add('dashboard__item__button dashboard__item__button--return>Devolver</a>');
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar' ;
     } else {
-        botao3.classList.remove('dashboard__item__button dashboard__item__button--return>Devolver</a>');
-        botao3.classList.add('dashboard__item__button>Alugar</a>');
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver' ;
     }
 }
